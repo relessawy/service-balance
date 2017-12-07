3372908d63425eaf81a9dd4b64682dcd49098214
apham <apham@redhat.com>
2017-12-07 23:57:49 +0100

    add prometheus configs

--- src/main/java/org/oasis_open/docs/wsrf/bf_2/BaseFaultType.java
+++ src/main/java/org/oasis_open/docs/wsrf/bf_2/BaseFaultType.java
@@ -113,8 +113,8 @@ public class BaseFaultType {
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link Object }
      * {@link Element }
+     * {@link Object }
      * 
      * 
      */
@@ -381,8 +381,8 @@ public class BaseFaultType {
          * 
          * @return
          *     possible object is
-         *     {@link Object }
          *     {@link Element }
+         *     {@link Object }
          *     
          */
         public Object getAny() {
@@ -394,8 +394,8 @@ public class BaseFaultType {
          * 
          * @param value
          *     allowed object is
-         *     {@link Object }
          *     {@link Element }
+         *     {@link Object }
          *     
          */
         public void setAny(Object value) {
